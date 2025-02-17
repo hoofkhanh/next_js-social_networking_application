57cd418c27abfceb86ded5e1fa79ddde088e3221
Ho Tuan Khanh <hotuankhanh20112016@gmail.com>
2025-02-17 13:30:36 +0700

    up

---
+++ app/tasks/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TaskPage = async () => {
+  const response = await fetch("http://localhost:3000/api/tasks", {
+    cache: "no-store",
+  });
+  const tasks = await response.json();
+
+  // console.log("tasks:", tasks);
+
+  return <div>TaskPage</div>;
+};
+
+export default TaskPage;
